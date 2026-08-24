cea558f438cb051094df599f184fe7bedff5470e
Vinicius Nery <vinicius_nery2@hotmail.com>
2026-08-24 13:55:15 -0300

    utilizando as classes e os metodos criados

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using ModelandoOficinaAutomobilistica.Modelos;
+
+Veiculo carro1 = new Veiculo("Chevrolet", "Onix", 2021, "FAT5L35");
+Cliente cliente1 = new Cliente("Vinicius", "666 UM TAPA NA OREIA");
+Mecanico mecanico1 = new Mecanico("Nelsinho", "Funileiro");
+Oficina oficinaDoNelsinho = new();
+
+oficinaDoNelsinho.AgendarServico(carro1, cliente1, mecanico1, "25/09/2030");
+oficinaDoNelsinho.RealizarServico(carro1, mecanico1);
